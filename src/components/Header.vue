2b04e78493a29fe73050debf16280f94a267df76
Andy Huang <andy.huang7@student.tdsb.on.ca>
2024-11-21 14:37:35 -0500

    added the components files and the router and views folders

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+<template>
+    <div class = header>
+        <h1>NotNetflix</h1>
+    </div>
+</template>
+<style scoped>
+h1 {
+    color: white;
+    text-align: center;
+    font-size: 50px;
+}
+.header {
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    font-weight: 400;
+    font-style: italic;
+    background-color: black;
+    text-align: center;
+    width: auto;
+}
+</style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,31 @@
+<script setup>
+</script>
+<template> 
+    <div class="Hero-section">
+        <div class="text">
+            <p>Welcome to NotNetflix</p>
+        </div>
+
+        <button class="herobuttons">Browse Movies</button>
+        <button class="herobuttons2">Trending Movies</button>
+    </div>
+</template>
+<style scoped>
+.Hero-section {
+    position: relative;
+    color: white;
+}
+.text {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 55px;
+    font-family: "Bebas Neue", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+}
+.Hero {
+    width: 100%;
+}
+</style>
