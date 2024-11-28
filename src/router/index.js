5635ee43c0713e63e78fa6e7d83af6991678e00b
Andy Huang <andy.huang7@student.tdsb.on.ca>
2024-11-28 17:03:19 -0500

    Added a Genres file and 3 new views

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import Home from '../views/Home.vue';
+import Login from '../views/Login.vue';
+import Movies from '../views/Movies.vue';
+import Register from '../views/Register.vue';
 
 const routes = [
     { path: '/', component: Home },
+    { path: '/login', component: Login },
+    { path: '/movies', component: Movies },
+    { path: '/register', component: Register },
 ]
 
 const router = createRouter({
